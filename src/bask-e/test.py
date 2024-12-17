68243c4f817a7774ac7c1eeffefa3bf3e8143d93
Rayane <rayane.zekik@epita.fr>
2024-12-17 18:45:09 +0100

    fix

--- src/bask-e/test.py
+++ src/bask-e/test.py
@@ -3,22 +3,23 @@ import paho.mqtt.client as mqtt
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, reason_code, properties):
     print(f"Connected with result code {reason_code}")
-    # Subscribe to all topics (wildcard '#')
-    client.subscribe("#")  # Subscribe to all topics
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe("nfc/card/read")
+    client.subscribe("scale/weight")
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    print(f"Topic: {msg.topic} | Message: {str(msg.payload.decode('utf-8'))}")
+    print(msg.topic+" "+str(msg.payload))
 
-# Create an MQTT client instance
 mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
-
-# Assign callback functions
 mqttc.on_connect = on_connect
 mqttc.on_message = on_message
 
-# Connect to the MQTT broker
 mqttc.connect("mqtt.eclipseprojects.io", 1883, 60)
 
-# Start the MQTT client loop to handle network traffic and dispatch callbacks
+# Blocking call that processes network traffic, dispatches callbacks and
+# handles reconnecting.
+# Other loop*() functions are available that give a threaded interface and a
+# manual interface.
 mqttc.loop_forever()
