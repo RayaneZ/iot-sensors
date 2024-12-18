88da588d2f183fcc4987c6740e2f5815ecaa3933
Pierre <pierre.meiss@epita.fr>
2024-12-18 14:13:40 +0100

    fix : install

--- src/install.sh
+++ src/install.sh
@@ -41,6 +41,8 @@ echo "Activation et démarrage des services..."
 for service in "$INSTALL_DIR"/services/etc/systemd/system/*.service; do
     service_name=$(basename "$service")
     echo "Activation du service : $service_name"
+    systemctl stop "$service_name"
+    systemctl disable "$service_name"
     systemctl enable "$service_name"
     systemctl start "$service_name"
 done
